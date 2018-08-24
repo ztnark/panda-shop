a93a1069155547a746e9b821702b6b85fd423fc7
Jeff Krantz <jjkrantz@gmail.com>
2018-08-24 16:49:46 -0500

    mvp

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -4,6 +4,12 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: 'rinkeby.infura.io/KlNoGrmiL7INbW6VuqlB',
+      port: 80,
+      network_id: 4,
+      gas: 4612388
     }
   }
 };
